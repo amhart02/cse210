569ffaceceabbc85b2a32a411f6c997d6ba1d1bd
amhart02 <har20010@byui.edu>
2024-06-22 14:07:47 -0600

    finished up journal program

--- prove/Develop02/Journal.cs
+++ prove/Develop02/Journal.cs
@@ -1,3 +1,6 @@
+using System;
+using System.Collections.Generic;
+using System.IO;
 public class Journal
 {
     public List<Entry> _entries = new List<Entry> ();
@@ -6,7 +9,43 @@ public class Journal
     {
         foreach (Entry entry in _entries)
         {
-            Console.WriteLine(entry);
+            Console.WriteLine(entry._date);
+            Console.WriteLine(entry._prompt);
+            Console.WriteLine(entry._response);
+            Console.WriteLine();
         }
+    }
+    public static void SaveToFile(Journal journal)
+    {
+        Console.WriteLine("Please provide a file name.");
+        string fileName = Console.ReadLine();
+
+        using (StreamWriter outputFile = new StreamWriter(fileName))
+        {
+            foreach (Entry entry in journal._entries)
+            {
+                outputFile.WriteLine(entry._date);
+                outputFile.WriteLine(entry._prompt);
+                outputFile.WriteLine(entry._response);
+                outputFile.WriteLine();
+            }
+        }
+    }
+    public static List<Entry> ReadFromFile()
+    {
+        List<Entry> entries = new List<Entry>();
+        Console.WriteLine("Please provide a file name.");
+        Console.WriteLine();
+        
+        string filename = Console.ReadLine();
+
+        string[] lines = System.IO.File.ReadAllLines(filename);
+
+        foreach (string line in lines)
+        {
+            Console.WriteLine(line);
+        }
+
+        return entries;
     }
 }
--- prove/Develop02/Program.cs
+++ prove/Develop02/Program.cs
@@ -1,5 +1,6 @@
 using System;
 using System.Reflection.Metadata;
+using System.Security.Cryptography.X509Certificates;
 
 class Program
 {
@@ -10,6 +11,12 @@ class Program
         DateTime theCurrentTime = DateTime.Now;
         string dateText = theCurrentTime.ToShortDateString();
         List<string> prompts = new List<string>{"Who was the most interesting person I interacted with today?", "What was the best part of my day?", "How did I see the hand of the Lord in my life today?", "What was the strongest emotion I felt today?", "If I had one thing I could do over today, what would it be?"};
+        List<string> affirmations = new List<string>{"I am worthy of love and respect.","I believe in myself and my abilities.", "I am confident in my decisions.", "I deserve happiness and success.", "I am proud of who I am becoming.", "I am enough just as I am.", "I am capable of achieving my dreams."};
+        Journal journal1 = new Journal();
+        //make random affirmation
+        Random random2 = new Random();
+        int randomIndex2 = random2.Next(affirmations.Count);
+        string randomAffirmation = affirmations[randomIndex2];
         //Display the menu with a while loop and if statement
         do {
             Console.WriteLine("Welcome to the Journal Program!");
@@ -20,35 +27,55 @@ class Program
             Console.WriteLine("4-Save the journal");
             Console.WriteLine("5-Quit");
             userChoice = Console.ReadLine();
+            //write a new entry
             if (userChoice == "1")
             {
+                //make random prompt
                 Random random = new Random();
                 int randomIndex = random.Next(prompts.Count);
                 string randomPrompt = prompts[randomIndex];
-                Entry entry = new Entry();
-                entry._date = dateText;
-                entry._prompt = randomPrompt;
-                Console.WriteLine(entry._prompt);
-                entry._response = Console.ReadLine();
+                //create new entry
+                Entry entry1 = new Entry();
+                entry1._date = dateText;
+                entry1._prompt = randomPrompt;
+                Console.WriteLine();
+                Console.WriteLine(entry1._prompt);
+                entry1._response = Console.ReadLine();
+                Console.WriteLine();
+                //add to journal 
+                journal1._entries.Add(entry1);  
             }
+            //display the journal
             else if (userChoice == "2")
             {
-                
+                Console.WriteLine();
+                journal1.DisplayJournal();
+                Console.WriteLine($"Your positive affirmation: {randomAffirmation} :))))");
+                Console.WriteLine();
             }
+            //load the journal
             else if (userChoice == "3")
             {
-
+                Console.WriteLine();
+                List<Entry> newEntries = Journal.ReadFromFile();
+                foreach (Entry entry in newEntries)
+                {
+                    Console.WriteLine(entry);
+                }
             }
+            //save the journal
             else if (userChoice == "4")
             {
-
+                Journal.SaveToFile(journal1);
+                Console.WriteLine();
+                Console.WriteLine("Saved to file.");
+                Console.WriteLine();
             }
             else 
             {
                 Console.WriteLine("Please choose a number 1-5.");
+                Console.WriteLine();
             }
-        } while (userChoice != "5");
-
-        
+        } while (userChoice != "5");        
     }
 }
