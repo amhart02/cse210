f1335918156a2720bfb941b94d73481e2d689a2d
amhart02 <har20010@byui.edu>
2024-07-08 13:00:36 -0600

    did the prepare assignment

--- prepare/Learning05/Program.cs
+++ prepare/Learning05/Program.cs
@@ -1,9 +1,27 @@
 using System;
+using System.Drawing;
 
 class Program
 {
     static void Main(string[] args)
     {
-        Console.WriteLine("Hello Learning05 World!");
+        List<Shape> shapes = new List<Shape>();
+
+        Square square = new Square(1.4, "red");
+        shapes.Add(square); 
+
+        Rectangle rectangle = new Rectangle(4, 6, "yellow");
+        shapes.Add(rectangle);
+
+        Circle circle = new Circle(4, "blue");
+        shapes.Add(circle);
+    
+        foreach (Shape shape in shapes)
+        {
+            string color = shape.GetColor();
+            double area = shape.GetArea();
+
+            Console.WriteLine($"Color: {color}, Area: {area}");
+        }
     }
 }
